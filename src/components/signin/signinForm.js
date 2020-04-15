b856fcb54105ea887b7dd4458ba5044dc81ec0f3
sdf <dfvsdfvadv>
2020-04-15 10:32:27 +0200

    change reset file Name and add reste Component

--- src/components/signin/signinForm.js
+++ src/components/signin/signinForm.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import TextField from '@material-ui/core/TextField';
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 import Zoom from 'react-reveal/Zoom'
-import PasswordReset from './passwordReset';
+import PasswordReset from './resetModal';
 
 export default function SigninForm(props) {
 
-    const{classes,submitHandler, changeHandler, email, errors, pass, handleClose, handleOpen, show} = props
+    const { classes, submitHandler, changeHandler, email, errors, pass, handleClose, handleOpen, show } = props
 
     return (
         <div>
@@ -26,7 +26,7 @@ export default function SigninForm(props) {
                                 value={email}
                                 onChange={changeHandler}
                             />
-                            {errors.email? errors.email.status?<smail className="sText"><p>{errors.email.value}</p></smail>:null: null}
+                            {errors.email ? errors.email.status ? <smail className="sText"><p>{errors.email.value}</p></smail> : null : null}
                             <TextField
                                 id="standard-password-input"
                                 label="Password"
@@ -38,12 +38,12 @@ export default function SigninForm(props) {
                             />
 
                             <button className="btn btn-primary mt-4 mb-2" type="submit">Sign in</button>
-                            {errors.form? errors.form.status?<smail className="sText"><p>{errors.form.value}</p></smail>:null: null}
-                            {errors.authentication? errors.authentication.status?<smail className="sText"><p>{errors.authentication.value}</p></smail>:null: null}
+                            {errors.form ? errors.form.status ? <smail className="sText"><p>{errors.form.value}</p></smail> : null : null}
+                            {errors.authentication ? errors.authentication.status ? <smail className="sText"><p>{errors.authentication.value}</p></smail> : null : null}
 
                             <small className="mt-3 myText">You are not registered yet? <Link to="/signup">Signup</Link></small>
-                            <small className="mt-2 myText">Forgot Password? <strong className="text-danger" style={{cursor: "pointer"}} onClick={handleOpen}>Click here</strong></small>
-                            <PasswordReset handleClose={handleClose} handleOpen={handleOpen} show={show} classes={classes}/>
+                            <small className="mt-2 myText">Forgot Password? <strong className="text-danger" style={{ cursor: "pointer" }} onClick={handleOpen}>Click here</strong></small>
+                            <PasswordReset handleClose={handleClose} handleOpen={handleOpen} show={show} classes={classes} />
 
                         </div>
 
