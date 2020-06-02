4ff9644f48468b12ee0ae64c6892037b948c847a
sdf <dfvsdfvadv>
2020-06-02 12:10:09 +0200

    Solve Activity View and Footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -5,33 +5,24 @@ import '../styles/main.css';
 import logo from '../../logo/1.png'
 const MyFooter = (props) => {
     return (
-        <div className="footer">
-            <div className="footerLogo ">
+        <div className="footer p-2">
+            <div className="footerLogo col-lg-2 col-md-2 col-sm-12">
                 <img src={logo} alt="logo" />
             </div>
-            <div className="container">
-                <div className="footerInfo">
-                    <div className="footerBox col-lg-3 col-md-3 col-sm-12 col-xs-12">
-                        <Link className="footerLink" to='/sellitems'>Sell</Link>
-                        <Link className="footerLink" to='/buyitems'>Buy</Link>
-                        <Link className="footerLink" to='/contact'>Contact</Link>
-                    </div>
-                    <div className="footerBox col-lg-3 col-md-3 col-sm-12 col-xs-12">
-                        <Link className="footerLink" to='/account'>Profile</Link>
-                        <Link className="footerLink" to='/messages'>Message</Link>
-                        <Link className="footerLink" to='/signin'>Signin</Link>
-                        <Link className="footerLink" to='/signup'>Signup</Link>
-                    </div>
+            <div className=" footerInfo col-lg-4 col-md-4 col-sm-4">
+                <Link className="footerLink" to='/sellitems'>Sell</Link>
+                <Link className="footerLink" to='/buyitems'>Buy</Link>
+                <Link className="footerLink" to='/contact'>Contact</Link>
+                <Link className="footerLink" to='/signin'>Signin</Link>
+            </div>
 
-                    <div className="footerBox col-lg-3 col-md-3 col-sm-12 col-xs-12">
-                        <h3>Find us ...</h3>
-                        <a className="footerLink" href='https://github.com/Milad-mosadegh'>Milad Mosadegh</a>
-                        <a className="footerLink" href='https://github.com/atherahmad'>Ather Ahmad</a>
-
-                    </div>
-                </div>
+            <div className="footerInfo col-lg-4 col-md-4 col-sm-4">
+                <h5 className="mt-2">&copy; {new Date().getFullYear()} Copyright :</h5>
+                <a className="footerLink2 ml-2" href='https://github.com/Milad-mosadegh'>Milad Mosadegh</a>
+                <a className="footerLink2 ml-2" href='https://github.com/atherahmad'>Ather Ahmad</a>
             </div>
         </div>
+
 
     );
 }
