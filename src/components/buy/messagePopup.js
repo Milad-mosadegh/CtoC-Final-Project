5d3bdaae333646c726d9aa4f85f96e8266cd1952
sdf <dfvsdfvadv>
2020-06-02 14:25:43 +0200

    somthing

--- src/components/buy/messagePopup.js
+++ src/components/buy/messagePopup.js
@@ -3,42 +3,44 @@ import TextField from '@material-ui/core/TextField';
 
 import '../styles/main.css'
 
-function ProductMessage({ proceedHandler, hideAlertBox, simpleAlert, cancelButtonBody }) {
+function ProductMessage({ proceedHandler, hideAlertBox, pdmShow, cancelButtonBody }) {
     return (
         <div className="pdm">
-            <div className="pdm-head">
-            </div>
-            <div className="pdm-head-title">
-                Message
-            </div>
+            <div className="pdm-content">
 
-            <div className="pdm-tilte">
-                <TextField
-                    id="outlined-multiline-static"
-                    label="Message"
-                    name="description"
-                    // value={product.description}
-                    // onChange={changeHandler}
-                    multiline
-                    rows={4}
-                    placeholder="Write Your Message Here"
-                    variant="outlined"
-                    className="mb-3 col-lg-12 col-md-12 col-sm-12"
-                    fullWidth={true}
-
-                />
-            </div>
-            {simpleAlert ?
-                <button type="button" className="myBlueButton-sm" onClick={hideAlertBox}>Ok</button>
-                :
-                <div className="alertBox-body">
-                    <button type="button" className="myBlueButton-sm" onClick={proceedHandler}>Yes   </button>
-
-                    {cancelButtonBody ?
-                        <button type="button" className="myRedButton-sm ml-1" onClick={hideAlertBox}>{cancelButtonBody}</button>
-                        : <button type="button" className="myRedButton-sm ml-1" onClick={hideAlertBox}>Cancel</button>}
+                <div className="pdm-head"></div>
+                <div className="pdm-head-title">
+                    Message
                 </div>
-            }
+
+                <div className="pdm-tilte">
+                    <TextField
+                        id="outlined-multiline-static"
+                        label="Message"
+                        name="description"
+                        // value={product.description}
+                        // onChange={changeHandler}
+                        multiline
+                        rows={4}
+                        placeholder="Write Your Message Here"
+                        variant="outlined"
+                        className="mb-3 col-lg-12 col-md-12 col-sm-12"
+                        fullWidth={true}
+
+                    />
+                </div>
+                {pdmShow ?
+                    <button type="button" className="myBlueButton-sm" onClick={hideAlertBox}>Ok</button>
+                    :
+                    <div className="alertBox-body">
+                        <button type="button" className="myBlueButton-sm" onClick={proceedHandler}>Yes   </button>
+
+                        {cancelButtonBody ?
+                            <button type="button" className="myRedButton-sm ml-1" onClick={hideAlertBox}>{cancelButtonBody}</button>
+                            : <button type="button" className="myRedButton-sm ml-1" onClick={hideAlertBox}>Cancel</button>}
+                    </div>
+                }
+            </div>
         </div>
     )
 }
