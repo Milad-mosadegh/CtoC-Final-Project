1c00cf09dfab525345557eab5f1d932affc0b8e9
sdf <dfvsdfvadv>
2020-08-24 12:59:56 +0200

    remove BE

--- backend/model/avatarModel.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-
-const avatarSchema = new mongoose.Schema({
-    img: { data: Buffer, contentType: String }
-})
-module.exports = mongoose.model("avatars", avatarSchema)
--- backend/router/dashboard.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const dashBoard   = require("../controller/dashboard")
-
-router.get("/dashboard", dashBoard.myAccount)
-
-
-module.exports = router;
