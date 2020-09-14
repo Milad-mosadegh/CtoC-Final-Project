c96aa862faa4113d032e44ac2ba1191cd4900736
sdf <dfvsdfvadv>
2020-09-14 17:52:12 +0200

    add basic Popup for Admin page

--- src/components/AdminPannel/PopupMessage/PopupAdmin.js
+++ src/components/AdminPannel/PopupMessage/PopupAdmin.js
@@ -1,11 +1,28 @@
 import React, { useState } from 'react'
 import '../../styles/main.css'
 
-function PopupAdmin({ popupAdminTitle, popupAdminBody }) {
+function AdminRedAlertBox({ adminRedBoxTitle, closeHandler, adminRedId, adminredBoxImage, adminRedCreator }) {
 
     return (
-  <div></div>
+        <div className="adminRedBox">
+            <div className="adminRedBox-head">
+                {/* <button style={{ float: "left", borderRadius: "100px", width: "50px", margin: "20px" }} onClick={closeHandler} className="btn-danger">X</button> */}
+            </div>
+            <div className="active-message-text">
+                <h1>{adminRedBoxTitle}</h1>
+            </div>
+
+            <div>
+                <img src={adminredBoxImage} width="200px" height="200px" />
+            </div>
+            <div className="adminredBox">
+                ID:{adminRedId}
+            </div>
+            <div className="adminredBox">
+                Creator:{adminRedCreator}
+            </div>
+        </div>
     )
 }
 
-export default PopupAdmin
+export default AdminRedAlertBox
